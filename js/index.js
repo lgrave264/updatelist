8b537a6b275dc6da367901679fbbb284f227e0e1
Luca Graves <Lgrave264@west-mec.org>
2021-01-22 21:44:16 -0700

    finish

--- js/index.js
+++ js/index.js
@@ -1,7 +1,7 @@
 
 var passList=[]; 
 class Passenger {
-    constructor(Id , Fname , Lname , Dob , Departure , Arrival , DepartureDate , ArrivalDate , Bags , Food , Extras){
+    constructor(Food , Extras){
         this.id = ID
         this.Fname = document.getElementById("Fname").value;
         this.Lname = document.getElementById("Lname").value;
@@ -11,17 +11,43 @@ class Passenger {
         this.DepartureDate = document.getElementById("DepartureDate").value;
         this.ArrivalDate = document.getElementById("ArrivalDate").value;
         this.Bags = document.getElementById("Bags").value;
-        this.Food = document.getElementById("Food").value;
-        this.Extras = document.getElementById("Extras").value;
+        this.Food = Food;
+        this.Extras = Extras;
+    }
+}
+var currentPass = null
+var ID = 1
+
+function create(Fname , Lname , Dob , Departure , Arrival , DepartureDate , ArrivalDate , Bags , Food , Extras){
+    
+
+var radios = document.getElementsByTagName('input');
+
+    document.getElementById("Food").value
+    var Food = null;
+    var Extras = null;
+    for (var i = 0; i < radios.length; i++) {
+        if(radios[i].checked && radios[i].id == "Food"){
+            Food = radios[i].value;
+        }
+        if(radios[i].checked && radios[i].id == "Extras"){
+            Extras = radios[i].value;
+        }
+    }
+
+    return new Passenger(Food , Extras);
+}
+function store(){
+ passList.push(create(Fname , Lname , Dob , Departure , Arrival , DepartureDate , ArrivalDate , Bags , Food , Extras));
+ ID++;
+}
+
+function search(Ids){
+    for(i = 0; i < passList.length; i++){
+        if(passList[i].id == document.getElementById("Ids").value){
+            currentPass = passList[i];
+            return passList[i];
+        }
     }
 }
 
-var ID = 0001
-
-function create(Fname , Lname , Dob , Departure , Arrival , DepartureDate , ArrivalDate , Bags , Food , Extras){
-    return new Passenger(Fname , Lname , Dob , Departure , Arrival , DepartureDate , ArrivalDate , Bags , Food , Extras);
-}
-function store(){
- passList.push(create(Fname , Lname , Dob , Departure , Arrival , DepartureDate , ArrivalDate , Bags , Food , Extras))
- ID ++
-}
